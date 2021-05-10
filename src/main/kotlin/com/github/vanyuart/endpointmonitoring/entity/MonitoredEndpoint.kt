28f50f42e4bf987f3e0632b0c15810a2e57374bf
Artem Vanyukhin <v96artem@gmail.com>
2021-05-10 20:45:08 +0200

    create domain model

--- src/main/kotlin/com/github/vanyuart/endpointmonitoring/entity/MonitoredEndpoint.kt
+++ src/main/kotlin/com/github/vanyuart/endpointmonitoring/entity/MonitoredEndpoint.kt
@@ -1,6 +1,5 @@
 package com.github.vanyuart.endpointmonitoring.entity
 
-import javax.persistence.CascadeType
 import javax.persistence.Entity
 import javax.persistence.ManyToOne
 import javax.persistence.OneToMany

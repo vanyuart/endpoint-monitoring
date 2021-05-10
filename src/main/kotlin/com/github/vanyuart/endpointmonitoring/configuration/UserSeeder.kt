9af97360d57602dff6d770e98e560580c9901cee
Artem Vanyukhin <v96artem@gmail.com>
2021-05-10 20:02:51 +0200

    move user seed to application.properties

--- src/main/kotlin/com/github/vanyuart/endpointmonitoring/configuration/UserSeeder.kt
+++ src/main/kotlin/com/github/vanyuart/endpointmonitoring/configuration/UserSeeder.kt
@@ -3,6 +3,7 @@ package com.github.vanyuart.endpointmonitoring.configuration
 import com.github.vanyuart.endpointmonitoring.entity.User
 import com.github.vanyuart.endpointmonitoring.repository.UserRepository
 import com.github.vanyuart.endpointmonitoring.util.logger
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.event.ContextRefreshedEvent
 import org.springframework.context.event.EventListener
@@ -10,13 +11,24 @@ import org.springframework.context.event.EventListener
 @Configuration
 class UserSeeder(
     private val userRepository: UserRepository,
+
+    @Value("\${endpointmonitoring.seed.user.username}")
+    private val username: String,
+    @Value("\${endpointmonitoring.seed.user.accessToken}")
+    private val accessToken: String,
+
+    @Value("\${endpointmonitoring.seed.user.username}")
+    private val username2: String,
+    @Value("\${endpointmonitoring.seed.user.accessToken}")
+    private val accessToken2: String,
 ) {
     private val log by logger()
 
+
     @EventListener
     fun seed(event: ContextRefreshedEvent) {
-        createUserIfNotExists(username = "user", accessToken = "bla")
-        createUserIfNotExists(username = "user2", accessToken = "bla2")
+        createUserIfNotExists(username = username, accessToken = accessToken)
+        createUserIfNotExists(username = username2, accessToken = accessToken2)
     }
 
     private fun createUserIfNotExists(username: String, accessToken: String) {

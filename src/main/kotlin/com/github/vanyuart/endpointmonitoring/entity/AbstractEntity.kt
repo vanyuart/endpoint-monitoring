d7a1d5acfe9f5d660f7b4631a40b659c1c43ee9a
Artem Vanyukhin <v96artem@gmail.com>
2021-05-12 22:09:03 +0200

    add AuditingEntityListener

--- src/main/kotlin/com/github/vanyuart/endpointmonitoring/entity/AbstractEntity.kt
+++ src/main/kotlin/com/github/vanyuart/endpointmonitoring/entity/AbstractEntity.kt
@@ -1,10 +1,14 @@
 package com.github.vanyuart.endpointmonitoring.entity
 
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.io.Serializable
 import java.time.ZonedDateTime
 import javax.persistence.*
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
 abstract class AbstractEntity(
 
     @Id
@@ -12,8 +16,10 @@ abstract class AbstractEntity(
     var id: Long = 0,
 
     @Column(nullable = false, updatable = false)
+    @CreatedDate
     var createdDate: ZonedDateTime = ZonedDateTime.now(),
 
     @Column
+    @LastModifiedDate
     var changedDate: ZonedDateTime? = null,
 ) : Serializable

c167eeee5329cb91d660d9c88bd7467c59227d7d
Artem Vanyukhin <v96artem@gmail.com>
2021-05-13 22:27:15 +0200

    create EndpointCheckerScheduledTask

---
+++ src/main/kotlin/com/github/vanyuart/endpointmonitoring/configuration/SchedulingConfig.kt
@@ -0,0 +1,9 @@
+package com.github.vanyuart.endpointmonitoring.configuration
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
+
+@Configuration
+@EnableScheduling
+class SchedulingConfig {
+}
---
+++ src/main/kotlin/com/github/vanyuart/endpointmonitoring/scheduled/EndpointCheckerScheduledTask.kt
@@ -0,0 +1,53 @@
+package com.github.vanyuart.endpointmonitoring.scheduled
+
+import com.github.kittinunf.fuel.httpGet
+import com.github.vanyuart.endpointmonitoring.entity.MonitoredEndpoint
+import com.github.vanyuart.endpointmonitoring.service.MonitoredEndpointService
+import com.github.vanyuart.endpointmonitoring.service.MonitoringResultService
+import com.github.vanyuart.endpointmonitoring.util.logger
+import kotlinx.coroutines.runBlocking
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+import java.time.ZonedDateTime
+
+/**
+ * In the real world project I would use Quartz scheduler as it provides better structure to the scheduled tasks.
+ * Here with only one scheduled task this should be sufficient.
+ *
+ * This task uses Fuel Http Client to asynchronously send http request using Kotlin coroutines.
+ */
+@Component
+class EndpointCheckerScheduledTask(
+    private val monitoredEndpointService: MonitoredEndpointService,
+    private val monitoringResultService: MonitoringResultService,
+) {
+    private val log by logger()
+
+    @Scheduled(cron = "\${endpointmonitoring.scheduled.endpointChecker.cron}")
+    fun executeTask() = runBlocking {
+        log.info("EndpointCheckerScheduledTask started")
+
+        val endpoints: List<MonitoredEndpoint> = monitoredEndpointService.getEndpointsForNextCheck()
+        endpoints.forEach { endpoint ->
+            val dateOfCheck = ZonedDateTime.now()
+
+            log.info("GET: ${endpoint.url}")
+            endpoint.url.httpGet().response { request, response, result ->
+
+                log.info("${response.statusCode} - ${endpoint.url}")
+
+                monitoringResultService.save(
+                    statusCode = response.statusCode,
+                    payload = response.responseMessage,
+                    dateOfCheck = dateOfCheck,
+                    endpoint = endpoint
+                )
+
+                monitoredEndpointService.updateNextCheckDate(endpoint.id, dateOfCheck)
+            }
+
+        }
+
+        log.info("EndpointCheckerScheduledTask finished.")
+    }
+}

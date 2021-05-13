67cf1fd7f5c8bf16c6233d8e4d66e9897029f3a9
Artem Vanyukhin <v96artem@gmail.com>
2021-05-13 22:29:33 +0200

    create MonitoringResultRepository

---
+++ src/main/kotlin/com/github/vanyuart/endpointmonitoring/repository/MonitoringResultRepository.kt
@@ -0,0 +1,11 @@
+package com.github.vanyuart.endpointmonitoring.repository
+
+import com.github.vanyuart.endpointmonitoring.entity.MonitoredEndpoint
+import com.github.vanyuart.endpointmonitoring.entity.MonitoringResult
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface MonitoringResultRepository : JpaRepository<MonitoringResult, Long> {
+    fun findTop10ByEndpoint(endpoint: MonitoredEndpoint): List<MonitoringResult>
+}

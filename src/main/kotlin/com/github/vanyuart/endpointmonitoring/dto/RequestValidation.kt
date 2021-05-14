ccc6efe696d2f763e61b3e1ef727365c69cc2a71
Artem Vanyukhin <v96artem@gmail.com>
2021-05-14 10:05:00 +0200

    RequestValidation - update url regex

--- src/main/kotlin/com/github/vanyuart/endpointmonitoring/dto/RequestValidation.kt
+++ src/main/kotlin/com/github/vanyuart/endpointmonitoring/dto/RequestValidation.kt
@@ -4,7 +4,7 @@ object RequestValidation {
     const val ENDPOINT_NAME_MSG = "Endpoint name should not be blank."
 
     const val URL_PATTERN_MSG = "URL does not have valid format, e.g. 'http(s)://example.com'."
-    const val URL_PATTERN_REGEX = "https?://(www\\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)"
+    const val URL_PATTERN_REGEX = "(?:^|[ \\t])((https?:\\/\\/)(?:localhost|[\\w-]+(?:\\.[\\w-]+)+)(:\\d+)?(\\/\\S*)?)"
 
     const val MONITORING_INTERVAL_MSG = "Monitoring interval should be greater than zero."
 }
